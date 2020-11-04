945b8f21387aaa29dbdb5bb1ccf742123d6e8e63
Pedram Ahmadpour Keyvani <ahmadpour.keyvani@gmail.com>
2020-11-04 02:38:48 +0100

    AppComponent and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from '@app/app.component';
+import { AuthGuardService } from '@app/auth/auth-guard.service';
 
 
 const routes: Routes = [
-  { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "login", loadChildren: () => import('@app/login/login.module').then(m => m.LoginModule) },
-  { path: "**", loadChildren: () => import('@app/not-found/not-found.module').then(m => m.NotFoundModule) }
+  { path: '', component: AppComponent, pathMatch: 'full', canActivate: [AuthGuardService] },
+  { path: 'login', loadChildren: () => import('@app/login/login.module').then(m => m.LoginModule) },
+  { path: 'contacts', loadChildren: () => import('@app/contact/contact.module').then(m => m.ContactModule) },
+  { path: '404', loadChildren: () => import('@app/not-found/not-found.module').then(m => m.NotFoundModule) },
+  { path: '**', loadChildren: () => import('@app/not-found/not-found.module').then(m => m.NotFoundModule) }
 ];
 
 @NgModule({
