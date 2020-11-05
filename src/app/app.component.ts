eb1a57cbb6c161ab2ecdde5e6eca469d73e841f2
Pedram Ahmadpour Keyvani <ahmadpour.keyvani@gmail.com>
2020-11-05 03:55:09 +0100

    Logout was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@app/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'uni-micro';
+  title = 'Uni Micro';
+
+  constructor(public authService: AuthService) { }
+
+  async onLogout() {
+    await this.authService.logout();
+  }
+
 }
